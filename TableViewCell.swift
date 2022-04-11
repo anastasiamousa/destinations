193e4049578727673ab335c3dc21eaf6557810a9
Anastasia Mousa <49337566+anastasiamousa@users.noreply.github.com>
2022-04-11 10:36:21 +0300

    Initial commit

---
+++ TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  destinations
+//
+//  Created by Anastasia Mousa on 11/4/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageCell: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
