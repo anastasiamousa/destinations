48f774069aa3badf0fdd96c3027b754c5666b89d
Anastasia Mousa <49337566+anastasiamousa@users.noreply.github.com>
2022-04-11 13:09:41 +0300

    additions

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -1,29 +1,54 @@
-//
 //  DetailViewController.swift
 //  destinations
-//
-//  Created by Anastasia Mousa on 11/4/22.
-//
+//  Created by Anastasia Mousa on 08/4/22.
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register (UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let details: [String]
+    
+    //Init
+    init(details: [String]) {
+        self.details = details
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        view.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return details.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = details[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print(details[indexPath.row])
+    }
 }
--- ViewController.swift
+++ ViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 struct Country {
     var image: UIImage?
     var title: String?
-    //var details: [String]?
+    var details: String?
 }
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var data: [Country] = [Country(image: UIImage(named: "Athens"),    title: "Athens, Greece",
-                           Country(image: UIImage(named: "Paris"),     title: "Paris, France"),
-                           Country(image: UIImage(named: "Berlin"),    title: "Berlin, Germany"),
-                           Country(image: UIImage(named: "Amsterdam"), title: "Amsterdam, Netherlands"),
-                           Country(image: UIImage(named: "Brussels"),  title: "Brussels, Belgium"),
-                           Country(image: UIImage(named: "Dublin"),    title: "Dublin, Ireland"),
-                           Country(image: UIImage(named: "Madrid"),    title: "Madrid, Spain"),
-                           Country(image: UIImage(named: "Nicosia"),   title: "Nicosia, Cyprus"),
-                           Country(image: UIImage(named: "Rome"),      title: "Rome, Italy"),
-                           Country(image: UIImage(named: "Vienna"),    title: "Vienna, Austria"),]
+    var data: [Country] = [Country(image: UIImage(named: "Athens"),    title: "Athens, Greece",         details: "Athens is the capital of Greece"),
+                           Country(image: UIImage(named: "Paris"),     title: "Paris, France",          details: "Paris is the capital of France"),
+                           Country(image: UIImage(named: "Berlin"),    title: "Berlin, Germany",        details: "Berlin is the capital of Germany"),
+                           Country(image: UIImage(named: "Amsterdam"), title: "Amsterdam, Netherlands", details: "Amsterdam is the capital of The Netherlands"),
+                           Country(image: UIImage(named: "Brussels"),  title: "Brussels, Belgium",      details: "Brussels is the capital of Belgium"),
+                           Country(image: UIImage(named: "Dublin"),    title: "Dublin, Ireland",        details: "Dublin is the capital of Ireland"),
+                           Country(image: UIImage(named: "Madrid"),    title: "Madrid, Spain",          details: "Madrid is the capital of Spain"),
+                           Country(image: UIImage(named: "Nicosia"),   title: "Nicosia, Cyprus",        details: "Nicosia is the capital of Cyprus"),
+                           Country(image: UIImage(named: "Rome"),      title: "Rome, Italy",            details: "Rome is the capital of Italy"),
+                           Country(image: UIImage(named: "Vienna"),    title: "Vienna, Austria",        details: "Vienna is the capital of Austria"),]
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -50,8 +50,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //ViewController.Country = self.data[indexPath.row]
+        let country = self.data[indexPath.row]
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = DetailViewController(details: data.details)
+        vc.title = data.title
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
